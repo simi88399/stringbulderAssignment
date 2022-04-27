36714393c713bcd116629447ba5bbe475e2f84ea
Harpreet Chahal <simichahal88@gmail.com>
2022-04-27 15:26:28 +0530

    StringBuilder Append

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace StringBuilders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "[{\"TaxTypeName\":\"QST\",\"Percentage\":4.000},{\"TaxTypeName\":\"MSTATTax\",\"Percentage\":5.000}]";
+
+
+
+            var details = JsonConvert.DeserializeObject<dynamic>(str);
+            var builder = new StringBuilder();
+            foreach (var item in details)
+            {
+                builder.Append($"{item.TaxTypeName}:{item.Percentage}+{item.TaxTypeName}: {item.Percentage}");
+
+            };
+            Console.WriteLine(builder);
+
+
+            Console.ReadLine();
+        }
+    }
+    }
+
